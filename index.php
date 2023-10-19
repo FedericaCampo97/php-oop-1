fec51b496ac00ff5315493dae97c8f5637303549
FedericaCampo97 <federica.campo97@gmail.com>
2023-10-19 12:27:43 +0200

    Init class and variables

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<?php
+// è definita una classe ‘Movie’ => 
+// all’interno della classe sono dichiarate delle variabili d’istanza => 
+// all’interno della classe è definito un costruttore => 
+// all’interno della classe è definito almeno un metodo
+
+// vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+
+
+class Movie
+{
+    public $title;
+    public $duration;
+    public $image;
+    public $genre;
+}
+?>
